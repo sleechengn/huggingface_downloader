61b810ac497bfd016ef9ba7f441a2354be606947
lee <sleechengn@live.cn>
2026-08-21 08:54:02 +0800

    sav

--- hfdl.sh
+++ hfdl.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+WORK_PATH=$(realpath $(pwd))
+SCRIPT_PATH=$(realpath $(dirname $0))
+
 if ! command -v aria2c > /dev/null 2>&1; then
     if command -v apt > /dev/null 2>&1; then
         apt install -y aria2
     fi
 fi
 
-if [ -e "$(dirname $0)/python/bin/activate" ]; then
-    . $(dirname $0)/python/bin/activate
-    HF_ENDPOINT=${HF_ENDPOINT:-https://hf-mirror.com} python $(dirname $0)/hf_downloader.py $*
+if [ -e "$WORK_PATH/python/bin/activate" ]; then
+    . $WORK_PATH/python/bin/activate
+    HF_ENDPOINT=${HF_ENDPOINT:-https://hf-mirror.com} python $SCRIPT_PATH/hf_downloader.py $*
 fi
